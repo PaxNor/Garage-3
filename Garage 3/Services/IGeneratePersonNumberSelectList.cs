84e3ab238ef089f77860cdf5dd0f560e9c042116
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-08-12 16:50:27 +0200

    Service for person number dropdown

---
+++ Garage 3/Services/IGeneratePersonNumberSelectList.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Garage_3.Services
+{
+    public interface IGeneratePersonNumberSelectList
+    {
+        Task<IEnumerable<SelectListItem>> GetMemberPersonNumberAsync();
+    }
+}
