a8fecbfc10eac2de73e98bdf3777c8b1de12804e
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-08-16 09:26:48 +0200

    Added navbar links, paginated list mm

--- Garage 3/ViewModels/ParkedVehiclesViewModel.cs
+++ Garage 3/ViewModels/ParkedVehiclesViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Garage_3.ViewModels
+﻿using Garage_3.Models;
+
+namespace Garage_3.ViewModels
 {
     public class ParkedVehiclesViewModel
     {
         // member
+
+        public IEnumerable<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PersNr { get; set; }
